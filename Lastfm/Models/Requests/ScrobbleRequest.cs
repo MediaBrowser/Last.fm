e767184fc3921b6a40e03700e106f270e9717a3b
hatharry <hatharry@hotmail.com>
2019-04-01 23:47:48 +1300

    add error handling

--- Lastfm/Models/Requests/ScrobbleRequest.cs
+++ Lastfm/Models/Requests/ScrobbleRequest.cs
@@ -14,14 +14,17 @@
 
         public override Dictionary<string, string> ToDictionary()
         {
-            return new Dictionary<string, string>(base.ToDictionary()) 
+            var dict = new Dictionary<string, string>(base.ToDictionary()) 
             {
                 { "track",     Track },
-                { "album",     Album },
                 { "artist",    Artist },
                 { "timestamp", Timestamp.ToString() },
-                { "mbid",      MbId }
             };
+            if (!string.IsNullOrWhiteSpace(Album))
+                dict.Add("album", Album);
+            if (!string.IsNullOrWhiteSpace(MbId))
+                dict.Add("mbid", MbId);
+            return dict;
         }
     }
 }
